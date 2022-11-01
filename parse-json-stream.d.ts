1234844b81101705258de2190c809721ae4abaa0
AndrewPerson <68579649+AndrewPerson@users.noreply.github.com>
2022-11-01 18:31:01 +1100

    Removed build artifacts

--- parse-json-stream.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function parseJSONStream(objectPaths: string[][]): {
-    write(buffer: Uint8Array): void;
-    finish(): void;
-    onObject(callback: (path: string[], value: any) => void): void;
-};
